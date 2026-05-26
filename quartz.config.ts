ad5f3270bdf1b477b3253d056470d2d2aec98998
CHOI <choi.jonny99@gmail.com>
2026-05-26 23:40:58 +0900

    Change Name

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "LLM Wiki",
+    pageTitle: "CHOI Wiki",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
